6d522e921130485140afd4a80b09b58f7c5a8ac2
Amr-Magdy95 <37376273+Amr-Magdy95@users.noreply.github.com>
2022-08-20 19:50:50 +0200

    feat: added express and sharp
    added and set express server.
    also added sharp for image processing.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 console.log('hello world');
 
 const myFunc = (num: number): number => {
-    return num * num;
-  };
-  
-  export default myFunc;
+  return num * num;
+};
+
+export default myFunc;
